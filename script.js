ca82494f470555464a8106a40a3500aeb867ef28
donski <136574676+Pedroso0417@users.noreply.github.com>
2025-08-18 16:31:41 +0800

    codes commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
- function goToWebsite() {
-      window.open('https://l.facebook.com/l.php?u=https%3A%2F%2Fforms.gle%2Fx95ksBRC1PbxuM8p6&h=AT1pnx_USNY5QAhPBsXbCK4NXMUdmE0xWAoajZewqwQgF4CmHowRj1it01ycfONDE_FnxFQZ-5RSv6ClM-zgAWsYbOEXU7YAnlDxkj_y1MAMDoAAHwN5dDHM9JA9_-M&s=1', '_blank');
-    }
+//  function goToImage() {
+//       open('https://l.facebook.com/l.php?u=https%3A%2F%2Fforms.gle%2Fx95ksBRC1PbxuM8p6&h=AT1pnx_USNY5QAhPBsXbCK4NXMUdmE0xWAoajZewqwQgF4CmHowRj1it01ycfONDE_FnxFQZ-5RSv6ClM-zgAWsYbOEXU7YAnlDxkj_y1MAMDoAAHwN5dDHM9JA9_-M&s=1', '_blank');
+//     }
